c03317ad4c6ae11b272ebe10e3bac9eb66f658ed
oihoihoih <ochyuo@hotmail.com>
2024-12-03 13:31:43 +0100

    Divider css animation triggered when scrolling

---
+++ src/javascript/about.js
@@ -0,0 +1,23 @@
+function scrollAnimation(selector) {
+  let elements = document.querySelectorAll(selector);
+  elements = Array.from(elements);
+  elements.forEach((element) => {
+    addObserver(element);
+  });
+}
+
+function addObserver(element) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animation");
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+  observer.observe(element);
+}
+
+scrollAnimation(".vintage-divider");
+
+// Code source: https://css-tricks.com/scroll-triggered-animation-vanilla-javascript/
