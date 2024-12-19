b5241e33807ea1bf9a5482aee7ed798168f8aaf9
oihoihoih <ochyuo@hotmail.com>
2024-12-19 10:46:43 +0100

    Postcard animation's animation-end improved

--- src/javascript/detail.js
+++ src/javascript/detail.js
@@ -3,30 +3,33 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// const postcards = document.querySelectorAll(".etape__wrapper");
-// const postcardHeight = postcards[0].offsetHeight;
-//
-// postcards.forEach((postcard, index) => {
-//   // animation.clear();
-//
-//   const tl = gsap
-//     .timeline()
-//     .from(postcard, {
-//       rotation: 0
-//     })
-//     .to(postcard, {
-//       rotation: gsap.utils.random(-8, 8),
-//       duration: 0.3
-//     });
-//
-//   ScrollTrigger.create({
-//     markers: true,
-//     trigger: postcard,
-//     start: "top top",
-//     pin: true,
-//     end: () => `+=${(postcards.length - index) * postcardHeight - postcardHeight}`,
-//     scrub: true,
-//     animation: tl,
-//     pinSpacing: false
-//   });
-// });
+const wrapper = document.querySelectorAll(".etape__wrapper");
+const postcards = document.querySelectorAll(".etape__postcard");
+//const wrapperHeight = wrapper[0].offsetHeight;
+const colors = ["red", "green", "blue", "yellow", "fuchsia", "pink", "orange"];
+const totalHeight = document.querySelector(".itinerary__wrapper").offsetHeight;
+
+postcards.forEach((postcard, index) => {
+  // animation.clear();
+  const prueba = postcard.querySelector(".etape__postcard");
+  const tl = gsap
+    .timeline()
+    .from(postcard, {
+      rotation: 0
+    })
+    .to(postcard, {
+      rotation: gsap.utils.random(-8, 8),
+      duration: 0.1
+    });
+
+  ScrollTrigger.create({
+    markers: { endColor: `${colors[index]}` },
+    trigger: postcard,
+    start: "top 20%",
+    pin: true,
+    end: () =>
+      `+=${totalHeight - wrapper[index].offsetHeight * index - wrapper[index].offsetHeight}`,
+    animation: tl,
+    pinSpacing: false
+  });
+});
